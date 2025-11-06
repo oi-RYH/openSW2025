819178323ec4379fe15f86ecf98717ab7b41bd26
oiryh <ryuyaelhwa@gmail.com>
2025-11-06 14:20:13 +0900

    Initial commit- foo, goo functions are defined

---
+++ src/func1.c
@@ -0,0 +1,7 @@
+#include "func1.h"
+#include <stdio.h>
+
+void foo(void)
+{
+    printf("This is foo.\n");
+}
---
+++ src/func2.c
@@ -0,0 +1,7 @@
+#include "func2.h"
+#include <stdio.h>
+
+void goo(void)
+{
+    printf("This is goo.\n");
+}
